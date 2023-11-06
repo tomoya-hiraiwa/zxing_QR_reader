7a25f2e471142ede51689b7fde8143c47225d57b
tomoya-hiraiwa <132980146+tomoya-hiraiwa@users.noreply.github.com>
2023-11-06 16:17:17 +0900

    first commit

---
+++ QrTest/app/src/main/java/com/example/qrtest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.qrtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+
+import com.example.qrtest.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var textData: String
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        permissionLauncher.launch(android.Manifest.permission.CAMERA)
+        binding.button.setOnClickListener {
+            val options = ScanOptions().apply {
+                setOrientationLocked(false)
+                setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+            }
+            barcodeLauncher.launch(options)
+        }
+    }
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){bool ->}
+    val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result ->
+        if (result.contents == null) {
+            // ここでQRコードを読み取れなかった場合の処理を書く
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            // ここでQRコードを読み取れた場合の処理を書く
+        }
+    }
+}
