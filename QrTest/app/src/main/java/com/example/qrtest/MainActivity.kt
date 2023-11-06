ed508e716f05f6856d42f2c1f16aa5a2d91bfffd
tomoya-hiraiwa <132980146+tomoya-hiraiwa@users.noreply.github.com>
2023-11-06 16:26:16 +0900

    change permissionLauncher Lambda

--- QrTest/app/src/main/java/com/example/qrtest/MainActivity.kt
+++ QrTest/app/src/main/java/com/example/qrtest/MainActivity.kt
@@ -19,16 +19,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //パーミッションランチャー呼び出し
         permissionLauncher.launch(android.Manifest.permission.CAMERA)
         binding.button.setOnClickListener {
+            //オプション指定
             val options = ScanOptions().apply {
                 setOrientationLocked(false)
+                //読み取る対象をQRのみに指定
                 setDesiredBarcodeFormats(ScanOptions.QR_CODE)
             }
+            //カメラ起動
             barcodeLauncher.launch(options)
         }
     }
-    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){bool ->}
+    //パーミッション確認
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){_ ->}
+    //カメラアプリ起動、読み取り後の処理
     val barcodeLauncher = registerForActivityResult(
         ScanContract()
     ) { result ->
